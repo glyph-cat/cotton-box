41d6ec5d43ac08d6a4edce5553a9050c7aafc432
glyph-cat <b75ggnacj@relay.firefox.com>
2024-05-07 22:30:20 +0800

    Stability enhancements for web examples

--- packages/examples-web/src/pages/api/open-in-editor/index.ts
+++ packages/examples-web/src/pages/api/open-in-editor/index.ts
@@ -9,6 +9,10 @@ export default async function APICancelSwapTicketHandler(
   // If 'code' is not found,
   // 1. Press ctrl/command-shift-P
   // 2. Search and run "Shell Command: Install 'code' command in PATH"
-  execSync(`code ${`${process.cwd()}/src/pages${path}.${fileExtension}`}`)
+  const constructedPath = `${process.cwd()}/src/pages${path}.${fileExtension}`
+  execSync(`code "${JSON.stringify(constructedPath)}"`)
+  // KIV: js/shell-command-injection-from-environment
+  // This is meant to be run temporarily in localhost only so it should be fine,
+  // but let's keep an eye on this issue just in case.
   res.status(200).send(null)
 }
