137d4e50dbea64bf0e4210a33ed599fddb2440f9
glyph-cat <b75ggnacj@relay.firefox.com>
2024-06-09 10:03:47 +0800

    Updated eslint config for web examples

---
+++ packages/examples-web/eslint.config.js
@@ -0,0 +1,12 @@
+const { recommended: baseRecommended } = require('@glyph-cat/eslint-config/base')
+const { recommended: reactRecommended } = require('@glyph-cat/eslint-config/react')
+
+module.exports = [
+  ...baseRecommended,
+  ...reactRecommended,
+  {
+    ignores: [
+      'src/pages/examples/',
+    ],
+  },
+]
