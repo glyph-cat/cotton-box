3d0910ed523e84d2131d0b52f56e49406462f65e
glyph-cat <b75ggnacj@relay.firefox.com>
2024-06-09 08:27:18 +0800

    Updated eslint config for react package

---
+++ packages/react/eslint.config.js
@@ -0,0 +1,15 @@
+const { Severity } = require('@glyph-cat/eslint-config')
+const { libraryAuthoring: baseLibraryAuthoring } = require('@glyph-cat/eslint-config/base')
+const { libraryAuthoring: reactLibraryAuthoring } = require('@glyph-cat/eslint-config/react')
+const { recommended: jestRecommended } = require('@glyph-cat/eslint-config/jest')
+
+module.exports = [
+  ...baseLibraryAuthoring,
+  ...jestRecommended,
+  ...reactLibraryAuthoring,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': Severity.OFF,
+    },
+  },
+]
