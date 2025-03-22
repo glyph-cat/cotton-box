2301ab1298203f1cdecedf682d0d0ac88d587eb8
glyph-cat <sprite-charter-0d@icloud.com>
2025-03-22 14:59:19 +0800

    Implemented setup script

--- scripts/run/setup/index.ts
+++ scripts/run/setup/index.ts
@@ -1,8 +1,24 @@
+import chalk from 'chalk'
 import { symlinkSync } from 'fs'
 
 function main(): void {
-  symlinkSync('./node_modules', './src/packages/core/node_modules')
-  symlinkSync('./node_modules', './src/packages/react/node_modules')
+
+  const cwd = process.cwd()
+
+  linkNodeModules(cwd, 'core')
+  linkNodeModules(cwd, 'react')
+
 }
 
 main()
+
+function linkNodeModules(cwd: string, packageName: 'core' | 'react'): void {
+  const node_modules = 'node_modules'
+  try {
+    symlinkSync(`${cwd}/${node_modules}`, `${cwd}/src/packages/${packageName}/node_modules`)
+    console.log(chalk.green(' ✓ ') + `Linked ${node_modules} to ${packageName} package`)
+  } catch (e) {
+    console.log(chalk.red(' × ') + `Failed to link ${node_modules} to ${packageName} package`)
+    console.error(e)
+  }
+}
