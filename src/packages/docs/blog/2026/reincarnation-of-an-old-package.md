f7ea2c6c8706056af5a20898f754fa73851bd7d3
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-03-15 14:38:41 +0800

    Updated docs

---
+++ src/packages/docs/blog/2026/reincarnation-of-an-old-package.md
@@ -0,0 +1,12 @@
+---
+slug: v1-is-finally-out
+title: V1 is finally out
+authors: glyphcat
+tags: []
+date: 2026-03-31
+# date still unsure actually
+---
+
+... 
+<!-- truncate -->
+...
---
+++ src/pages/examples/api/react/useSimpleStateValueOnly/basic.tsx
@@ -0,0 +1,33 @@
+import { SimpleStateManager } from 'cotton-box'
+import { useSimpleStateValueOnly } from 'cotton-box-react'
+import { ReactNode } from 'react'
+
+export default function App(): ReactNode {
+  const user = useSimpleStateValueOnly(UserState)
+  return (
+    <div>
+      <h1>Hello, {user.firstName} {user.lastName}!</h1>
+      <h2>Your lucky number is {user.luckyNumber || '...'}</h2>
+      <button onClick={rollLuckyNumber}>Roll lucky number</button>
+    </div>
+  )
+}
+
+interface IUserState {
+  firstName: string
+  lastName: string
+  luckyNumber: number
+}
+
+const UserState = new SimpleStateManager<IUserState>({
+  firstName: 'John',
+  lastName: 'Smith',
+  luckyNumber: 0,
+})
+
+function rollLuckyNumber(): void {
+  UserState.set((previousState) => ({
+    ...previousState,
+    luckyNumber: Math.round(Math.random() * 100),
+  }))
+}
