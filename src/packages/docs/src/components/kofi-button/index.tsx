c1230b33de0ef89bcbef5966fd8d30e0225aa727
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-03-20 13:52:51 +0800

    Fix: Kofi panel overflow

--- src/packages/docs/src/components/kofi-button/index.tsx
+++ src/packages/docs/src/components/kofi-button/index.tsx
@@ -1,17 +1,30 @@
 import { AnimatePresence, motion } from 'motion/react'
-import { ReactNode, useCallback, useState } from 'react'
+import { ReactNode, useCallback, useInsertionEffect, useState } from 'react'
 import styles from './index.module.css'
 
+const IFRAME_PADDING_TOP = 42 // px
+
 export function KofiButton(): ReactNode {
-  const [showPanel, setPanelVisibility] = useState(false)
+  const [shouldShowPanel, setPanelVisibility] = useState(false)
+  const showPanel = useCallback(() => { setPanelVisibility(true) }, [])
   const hidePanel = useCallback(() => { setPanelVisibility(false) }, [])
+  useInsertionEffect(() => {
+    // Docusaurus injects an inline `overflow: visible;` to the document body.
+    console.log('shouldShowPanel', shouldShowPanel)
+    if (shouldShowPanel) {
+      document.body.classList.add(styles.noScroll)
+      document.body.style.overflow = 'hidden'
+      return () => {
+        document.body.style.overflow = 'visible'
+        document.body.classList.remove(styles.noScroll)
+      }
+    }
+  }, [shouldShowPanel])
   return (
     <>
       <button
         className={styles.button}
-        onClick={useCallback(() => {
-          setPanelVisibility(v => !v)
-        }, [])}
+        onClick={showPanel}
       >
         <img
           height={36}
@@ -21,58 +34,36 @@ export function KofiButton(): ReactNode {
         />
       </button>
       <AnimatePresence initial={false}>
-        {showPanel && (
-          <div
-            style={{
-              position: 'fixed',
-              top: 0,
-              left: 0,
-              height: '100vh',
-              width: '100vw',
-              placeItems: 'center',
-              display: 'grid',
-              zIndex: 1,
-            }}
-          >
+        {shouldShowPanel && (
+          <div className={styles.popupContainer}>
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              style={{
-                backgroundColor: '#00000080',
-                backdropFilter: 'blur(2px)',
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                height: '100vh',
-                width: '100vw',
-                placeItems: 'center',
-                display: 'grid',
-              }}
+              className={styles.underlay}
               onClick={hidePanel}
             />
             <motion.div
-              initial={{ opacity: 0, scale: 0 }}
+              className={styles.popupSheet}
+              initial={{ opacity: 0, scale: 0.85 }}
               animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0 }}
-              style={{
-                width: 500,
-                maxWidth: '95vw',
-                zIndex: 1,
-              }}
+              exit={{ opacity: 0, scale: 0.85 }}
             >
+              <div style={{
+                display: 'grid',
+                justifySelf: 'end',
+                height: IFRAME_PADDING_TOP,
+              }}>
+                <button
+                  className={styles.closeButton}
+                  onClick={hidePanel}
+                >×</button>
+              </div>
               <iframe
                 id='kofiframe'
                 src='https://ko-fi.com/glyphcat/?hidefeed=true&widget=true&embed=true&preview=true'
-                style={{
-                  backgroundColor: '#f9f9f9',
-                  borderRadius: 10,
-                  border: 0,
-                  padding: 4,
-                  paddingTop: 50,
-                  width: '100%',
-                }}
-                height='712'
+                className={styles.iframe}
+                height={712 + IFRAME_PADDING_TOP}
                 title='glyphcat'
               />
             </motion.div>
