c2ff850eed7614d336b100677f3018fbb3260276
glyph-cat <32414645+glyph-cat@users.noreply.github.com>
2026-06-26 18:56:10 +0800

    Updated web playground configs

--- packages/playground-web/next.config.ts
+++ packages/playground-web/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from 'next'
+import path from 'node:path'
 
 const nextConfig: NextConfig = {
   devIndicators: false,
   reactStrictMode: false,
+  turbopack: {
+    root: path.join(process.cwd(), '..', '..'),
+  },
   typescript: {
     ignoreBuildErrors: true,
   }
